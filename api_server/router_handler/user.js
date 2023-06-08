8f8cb15021ac8770fdc5125bb51cd1ac68d3aa4d
PXL <q1748003813@qq.com>
2023-06-08 14:28:36 +0800

    优化

--- api_server/router_handler/user.js
+++ api_server/router_handler/user.js
@@ -8,20 +8,17 @@ exports.regUser = (req, res) => {
 
   // 校验账号密码是否为空
   if (!userinfo.username || !userinfo.password) {
-    return res.send({ status: 1, message: '用户名或密码不能为空！' })
+    return res.cc('用户名或密码不能为空！')
   }
 
   // 查询账号是否重复
   const sqlStr = 'select * from my_db_01.ev_users where username=?'
 
   db.query(sqlStr, userinfo.username, (err, results) => {
-    if (err) return res.send({ status: 1, message: err.message })
+    if (err) return res.cc(err)
 
     if (results.length > 0) {
-      return res.send({
-        status: 1,
-        message: '当前用户名已存在，请更换其他用户名',
-      })
+      return res.cc('当前用户名已存在，请更换其他用户名')
     }
 
     // 密码加密
@@ -34,13 +31,13 @@ exports.regUser = (req, res) => {
       sql,
       { username: userinfo.username, password: userinfo.password },
       (err, results) => {
-        if (err) return res.send({ status: 1, message: err.message })
+        if (err) return res.cc(err)
 
         if (results.affectedRows !== 1) {
-          return res.send({ status: 1, message: '注册用户失败，请稍后再试' })
+          return res.cc('注册用户失败，请稍后再试')
         }
 
-        res.send({ status: 0, message: '注册成功！' })
+        res.cc('注册成功！', 1)
       }
     )
   })
